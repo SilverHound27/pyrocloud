1d3182fa45d0bf92b39766751394e8eecbfa918c
SilverHound27 <62786183+SilverHound27@users.noreply.github.com>
2020-10-08 01:46:50 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,12 @@
+class Creds():
+    
+    TG_TOKEN = "1157928826:AAGE1lSPh6ZNzHCu5ogjKwouBrDbgXqejfI"
+
+    TEAMDRIVE_FOLDER_ID = "17wQMosfFyaZ6KkWdAnIqm_NwCqvxzARY"
+
+    TEAMDRIVE_ID = "0AFPU3EvaL3pRUk9PVA"
+
+    APP_ID = '1387390'
+
+    API_HASH = '4654be5adca2c279883b56cea9f0fa65'
+    

7f20eb44aa10f3c641980cb08d066a5e296b7097
SilverHound27 <jerryjo1127@gmail.com>
2020-10-16 12:11:45 +0530

    ngrok test

--- bot.py
+++ bot.py
@@ -21,6 +21,21 @@ app = Client(
         workers=343
     )
 
+def install_ngrok():
+    import os
+    from zipfile import ZipFile
+    from urllib.request import urlretrieve
+    
+    url = 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'
+    urlretrieve(url, 'ngrok-amd64.zip')
+    
+    with ZipFile('ngrok-amd64.zip', 'r') as zip_ref:
+        zip_ref.extractall('/usr/local/bin/')
+    os.chmod('/usr/local/bin/ngrok', 0o755)
+    os.unlink('ngrok-amd64.zip')
+    
+app.add_handler(pyrogram.handlers.MessageHandler(auth,filters=filters.command(['ngrok'])))
+
 #@app.on_message(filters.command(["qw"]))
 
 #The gdrive auth handler
